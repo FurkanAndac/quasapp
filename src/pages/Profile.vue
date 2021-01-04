7fa7287354686e1f6afc9dc64afb5ff60d9afbbf
FurkanAndac <furkan_andac_@hotmail.com>
2021-01-04 01:19:23 +0100

    Add getEmail, getProfile call for ProfilePage

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,12 +1,15 @@
 <template>
   <q-page padding>
     <!-- content -->
-    <div >
+    <!--  PANEL -->
+
+    
+    <!-- EDIT PANEL -->
+    <div class='myDIV'>
       PROFILE
-      <q-input class='q-pa-sm' filled v-model="email" input-class="text-right" label-slot clearable>
+      <q-input :readonly='readOnly' class='q-pa-sm' filled v-model="email" input-class="text-right" label-slot clearable>
         <template v-slot:label>
           <div class="row items-center all-pointer-events">
-            <q-icon class="q-mr-xs" color="teal" size="24px" name="mail" />
             Email adres
           </div>
         </template>
@@ -23,18 +26,63 @@
         filled
         type="file"
         hint="Curriculum Vitae"
-      />
+        />
     </div>
+    <q-btn @click="editForm" :label="editLabel" type="edit" color="teal"/>
+    <q-btn @click="getProfile" label="Submit" type="submit" color="teal"/>
   </q-page>
 </template>
 
 <script>
 export default {
   // name: 'PageName',
+  components: {
+  },
   data () {
     return {
       file: null,
+      profile: this.getProfile(),
+      readOnly: true,
+      editLabel: 'Bewerk',
+      email: this.getEmail(),
+
     }
+  },
+  mounted() {
+    this.editForm()
+  },
+  methods: {
+    getProfile() {
+      fetch("http://localhost:8080/api/users/5ff239dab218b73fb40bac84")
+                .then(response => response.json())
+                .then(data => (this.profile = data));
+    },
+    getEmail() {
+      fetch("http://localhost:8080/api/users/5ff239dab218b73fb40bac84")
+          .then(response => response.json())
+          .then(data => (this.email = data.data.email));
+    },
+    editForm() {
+      if(this.readOnly == true){
+        this.email = this.profile.data.email
+        this.readOnly = false;
+        this.editLabel = 'Opslaan'
+      } else {
+        this.readOnly = true
+        this.editLabel = 'Bewerk'
+        console.log(this.profile)
+      }
+
+    },
+    myFunction() {
+      var x = document.getElementById("myDIV");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    },
+
   }
 }
 </script>
